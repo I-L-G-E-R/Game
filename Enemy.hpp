93263786677689193d47dea3db912ea703edca5b
I-L-G-E-R <ilger2006@mail.ru>
2025-05-25 14:16:51 +0300

    made class Enemy

---
+++ Enemy.hpp
@@ -0,0 +1,13 @@
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+class ENEMY
+{
+   void set(Texture &image, int x, int y);
+   void update(float time);
+
+   void update(float time);
+
+   void Collision();
+}
