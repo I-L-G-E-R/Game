39bd293539f3f3888f44fb91d07c44647d72a565
alanprog777 <alan@DESKTOP-PPVR79R.>
2025-05-26 21:48:55 +0300

    in process

--- StartGame.hpp
+++ StartGame.hpp
@@ -6,10 +6,31 @@ using namespace sf;
 
 void StartGame();
 
-static float offsetX = 0;
-static float offsetY = 0;
-
 const int H = 17;
 const int W = 142;
 
-static String TileMap[H];
+
+namespace {
+float offsetX = 0;
+float offsetY = 0;
+
+String TileMap[H] = {
+"000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000P00000000000000",
+"0                                                                                                                                P             0",
+"0                                                                                                                                P             0",
+"0                   w                                  w                   w                                               w     P             0",
+"0                                      w                                                        w                                P             0",
+"0                                                                                                                                P             0",
+"0                      c                                                                                                         P             0",
+"0                                                                       r                                                        P             0",
+"0                                                                      rrr                                         q             P             0",
+"0                                                                     rrrrr                                                      P             0",
+"0               c    kckck                                           rrrrrrr                                                     P             0",
+"0                                      t0                           rrrrrrrrr                                                    P             0",
+"0G                                     00              t0          rrrrrrrrrrr                                                   P             0",
+"0           d    g       d             00              00         rrrrrrrrrrrrr                                                  P             0",
+"PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP",
+"PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP",
+"PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP",
+};
+}
